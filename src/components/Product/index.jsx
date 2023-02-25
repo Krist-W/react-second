39555a652a71e6322db796506c9d6fd2df7a1347
Krist-W <259720@mail.ru>
2023-02-25 10:18:28 +0300

    final ver 1

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -8,14 +8,14 @@ function Product(props) {
       <div className="flex flex cols-2">
        
 
-        <a
+        <p
           className="bg-gradient-to-r from-rose-500 hover:bg-rose-500 text-gray-800 font-semibold rounded-full p-3 m-3 shadow-md item-center"
           onClick={() => changeButton(product.id)}
         >
           {product.isChecked ? "Добавлено" : "Добавить"}
-        </a>
+        </p>
 
-        <a
+        <p
           className="text-gray-800 hover:text-rose-900 p-3 m-1 item-center"
           onClick={() => deleteProduct(product.id)}
         >
@@ -33,7 +33,7 @@ function Product(props) {
               d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
             />
           </svg>
-        </a>
+        </p>
         
       </div>
     </div>
